1c152e5a47e1dcbcc24a78487141fa58db9c1781
axemonkey <hi+github@clivemurray.com>
2017-08-15 16:58:26 +0100

    adds linting and moves vendor/gov js into vendor folder

--- gulp/watch.js
+++ gulp/watch.js
@@ -7,6 +7,7 @@
 var gulp = require('gulp')
 var config = require('./config.json')
 
+
 gulp.task('watch-sass', function () {
   return gulp.watch(config.paths.assets + 'sass/**', {cwd: './'}, ['sass'])
 })
@@ -15,3 +16,7 @@ gulp.task('watch-assets', function () {
   return gulp.watch([config.paths.assets + 'images/**',
     config.paths.assets + 'javascripts/**'], {cwd: './'}, ['copy-assets'])
 })
+
+gulp.task('watch-lint', function () {
+  return gulp.watch([config.paths.assets + 'javascripts/**', '.eslintrc.json'], {cwd: './'}, ['lint']);
+});
