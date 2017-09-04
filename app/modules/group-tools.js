2944da57c8ea31c8213a99562f0c6829b6132388
Clive Murray <hi+github@clivemurray.com>
2017-09-04 16:52:34 +0100

    WIP: start revamp group editing (#22)
    * start revamp group editing
    
    * parseInt bug found
    
    * remove member from group functionality
    
    * add apps to group functionality

--- app/modules/group-tools.js
+++ app/modules/group-tools.js
@@ -8,7 +8,7 @@ var _ = require('lodash'),
   apps = require('../assets/data/dummy-apps.json');
 
 var groupTools = {
-  addMembers: function() {
+  addMembers: function() { // add members to newly generated group
     var x,
       groupMembers = [],
       numMembers = Math.floor(Math.random() * constants.quantities.MAX_GROUP_MEMBERS) + 1;
@@ -22,12 +22,11 @@ var groupTools = {
 
     groupMembers = _.uniqBy(groupMembers, 'id');
     groupMembers = _.sortBy(groupMembers, 'id');
-
     groupMembers[0].role = 1;
 
     return groupMembers;
   },
-  addApps: function() {
+  addApps: function() { // add apps to newly generated group
     var x,
       groupApps = [],
       numApps = Math.floor(Math.random() * constants.quantities.MAX_GROUP_APPS) + 1;
@@ -40,6 +39,81 @@ var groupTools = {
 
     return groupApps;
   },
+  newMember: function(groupId, memberId) { // add member to group in memory only
+    var self = this,
+      group = self.getGroup(groupId),
+      groupIndex = _.findIndex(groups, {'id': parseInt(groupId, 10)}),
+      members = group.members;
+
+    members.push({
+      id: parseInt(memberId, 10),
+      role: 0
+    });
+    members = _.sortBy(members, 'id');
+    groups[groupIndex].members = members;
+
+    return true;
+  },
+  deleteMember: function(groupId, memberId) { // remove member from group in memory
+    var self = this,
+      group = self.getGroup(groupId),
+      groupIndex = _.findIndex(groups, {'id': parseInt(groupId, 10)}),
+      members = group.members,
+      member = _.find(members, {'id': parseInt(memberId, 10)}),
+      memberIndex = _.findIndex(members, {'id': parseInt(memberId, 10)}),
+      numAdmins = _.filter(members, {'role': 1}).length;
+
+    if (memberIndex === -1 || members.length === 1 || (numAdmins === 1 && member.role === 1)) {
+      return false; // got to have at least one admin member
+    }
+
+    _.remove(members, {'id': parseInt(memberId, 10)});
+    groups[groupIndex].members = members;
+
+    return true;
+  },
+  toggleAdminRole: function(groupId, memberId) {
+    var self = this,
+      group = self.getGroup(groupId),
+      groupIndex = _.findIndex(groups, {'id': parseInt(groupId, 10)}),
+      members = group.members,
+      member = _.find(members, {'id': parseInt(memberId, 10)}),
+      memberIndex = _.findIndex(members, {'id': parseInt(memberId, 10)}),
+      numAdmins = _.filter(members, {'role': 1}).length,
+      newRole = Math.abs(parseInt(member.role, 10) - 1);
+
+    if (newRole === 0 && numAdmins <= 1) {
+      return false; // can't have no admins in group
+    }
+
+    groups[groupIndex].members[memberIndex].role = newRole;
+
+    return true;
+  },
+  newApp: function(groupId, appId) { // add app to group in memory only
+    var self = this,
+      group = self.getGroup(groupId),
+      groupIndex = _.findIndex(groups, {'id': parseInt(groupId, 10)}),
+      apps = group.apps;
+
+    apps.push(parseInt(appId, 10));
+    apps = _.sortBy(apps, 'id');
+    groups[groupIndex].apps = apps;
+
+    return true;
+  },
+  deleteApp: function(groupId, appId) { // remove app from group in memory
+    var self = this,
+      group = self.getGroup(groupId),
+      groupIndex = _.findIndex(groups, {'id': parseInt(groupId, 10)}),
+      apps = group.apps;
+
+    _.pull(apps, parseInt(appId, 10));
+    groups[groupIndex].apps = apps;
+
+    return true;
+  },
+
   getGroup: function(id) {
     return _.find(groups, {'id': parseInt(id, 10)});
   },
--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var _ = require('lodash');
+var constants = require('./constants.json');
 var groupTools = require('./modules/group-tools.js');
 var appTools = require('./modules/app-tools.js');
 var userTools = require('./modules/user-tools.js');
@@ -9,7 +10,6 @@ var datasourceTools = require('./modules/datasource-tools.js');
 var users = require('./assets/data/dummy-users.json');
 var apps = require('./assets/data/dummy-apps.json');
 var groups = require('./assets/data/dummy-groups.json');
-var grouproles = require('./assets/data/group-roles.json');
 var datasources = require('./assets/data/dummy-datasources.json');
 
 // Route index page
@@ -45,7 +45,7 @@ router.get('/users/show/:index', function (req, res) {
   res.render('users/show', {
     user: user,
     userGroups: userGroups,
-    grouproles: grouproles
+    grouproles: constants.GROUP_ROLES
   });
 });
 
@@ -81,29 +81,82 @@ router.get('/groups/list', function (req, res) {
   });
 });
 router.get('/groups/edit/:index', function (req, res) {
-  var group = groupTools.getGroup(req.params.index),
-    members = groupTools.getGroupMembers(req.params.index),
-    groupApps = groupTools.getGroupApps(req.params.index);
+  var group = groupTools.getGroup(req.params.index);
 
   res.render('groups/edit', {
+    group: group
+  });
+});
+router.get('/groups/edit-members/:index', function (req, res) {
+  var group = groupTools.getGroup(req.params.index),
+    members = groupTools.getGroupMembers(req.params.index);
+
+  res.render('groups/edit-members', {
     group: group,
     members: members,
-    grouproles: grouproles,
-    usersNotInGroup: _.difference(users, members),
+    grouproles: constants.GROUP_ROLES,
+    usersNotInGroup: _.difference(users, members)
+  });
+});
+router.get('/groups/add-member/:index', function (req, res) {
+  if(groupTools.newMember(req.params.index, req.query['add-member'])) {
+    res.redirect('/groups/edit-members/' + req.params.index);
+  } else {
+    console.log('add member failed');
+  }
+});
+router.get('/groups/remove-member/:groupId/:memberId', function (req, res) {
+  if(groupTools.deleteMember(req.params.groupId, req.params.memberId)) {
+    res.redirect('/groups/edit-members/' + req.params.groupId);
+  } else {
+    console.log('remove member failed - group must contain one or more members, one or more of whom must be admins');
+    res.send('Remove member failed - group must contain one or more members, one or more of whom must be admins');
+  }
+});
+router.get('/groups/toggle-admin-role/:groupId/:memberId', function (req, res) {
+  if(groupTools.toggleAdminRole(req.params.groupId, req.params.memberId)) {
+    res.redirect('/groups/edit-members/' + req.params.groupId);
+  } else {
+    console.log('toggle member admin role failed');
+    res.send('Groups must contain at least one admin');
+  }
+});
+router.get('/groups/edit-apps/:index', function (req, res) {
+  var group = groupTools.getGroup(req.params.index),
+    groupApps = groupTools.getGroupApps(req.params.index);
+
+  res.render('groups/edit-apps', {
+    group: group,
     groupApps: groupApps,
     appsNotAvailableToGroup: _.difference(apps, groupApps)
   });
 });
+router.get('/groups/add-app/:index', function (req, res) {
+  if(groupTools.newApp(req.params.index, req.query['add-app'])) {
+    res.redirect('/groups/edit-apps/' + req.params.index);
+  } else {
+    console.log('add app failed');
+  }
+});
+router.get('/groups/remove-app/:groupId/:appId', function (req, res) {
+  if(groupTools.deleteApp(req.params.groupId, req.params.appId)) {
+    res.redirect('/groups/edit-apps/' + req.params.groupId);
+  } else {
+    console.log('remove app failed');
+    res.send('Remove app failed');
+  }
+});
+
 router.get('/groups/show/:index', function (req, res) {
   var group = groupTools.getGroup(req.params.index),
     members = groupTools.getGroupMembers(req.params.index),
-    apps = groupTools.getGroupApps(req.params.index);
+    groupApps = groupTools.getGroupApps(req.params.index);
 
   res.render('groups/show', {
     group: group,
     members: members,
-    apps: apps,
-    grouproles: grouproles
+    groupApps: groupApps,
+    grouproles: constants.GROUP_ROLES
   });
 });
 
