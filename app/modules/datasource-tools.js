404ae57ad3ca3e5dad0634bdec385f7ea96af747
axemonkey <hi+github@clivemurray.com>
2017-09-08 15:41:14 +0100

    add data source to app

--- app/modules/datasource-tools.js
+++ app/modules/datasource-tools.js
@@ -1,5 +1,6 @@
 'use strict';
 var _ = require('lodash'),
+  arrayTools = require('../modules/array-tools.js'),
   datasources = require('../assets/data/dummy-datasources.json'),
   apps = require('../assets/data/dummy-apps.json');
 
@@ -35,6 +36,33 @@ var datasourceTools = {
       bucket_name: formData.bucket_name,
       bucket_url: formData.bucket_url
     });
+
+    return true;
+  },
+  getAppsWithoutDatasource: function(datasourceId) {
+    var appsWithoutDatasource = [],
+      x;
+
+    for (x = 0; x < apps.length; x += 1) {
+      if (!apps[x].datasources.includes(datasourceId)) {
+        appsWithoutDatasource.push({
+          id: apps[x].id,
+          name: apps[x].name
+        });
+      }
+    }
+
+    return appsWithoutDatasource;
+  },
+  addDatasourceToApp: function(datasourceId, formData) {
+    var appId = parseInt(formData['add-datasource-to-app'], 10),
+      app = _.find(apps, {'id': appId}),
+      appIndex = _.findIndex(apps, {'id': appId}),
+      datasources = app.datasources;
+
+    datasources.push(parseInt(datasourceId, 10));
+    datasources = arrayTools.sort(datasources);
+    apps[appIndex].datasources = datasources;
 
     return true;
   }
