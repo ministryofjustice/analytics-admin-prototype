550cdc2047d4f53a4dc9de0a49d88a36f18d9093
axemonkey <hi+github@clivemurray.com>
2017-09-14 14:47:30 +0100

    park this for now

--- app/routes-user-home.js
+++ app/routes-user-home.js
@@ -1,13 +1,6 @@
 module.exports = function(router, users, apps, groups, datasources) {
   router.get('/home/:userId', function (req, res) {
-    // TODO: YOU ARE HERE
-    // don't need all this stuff, use more tools to get the relevant
-    // stuff per user
-    res.render('home/index', {
-      users: users,
-      apps: apps,
-      groups: groups,
-      datasources: datasources
-    });
+    // get stuff to show here
+    res.render('home/index');
   });
 }
