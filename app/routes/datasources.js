dbba24b0b5d28dd74eeac407e1df4b661ddfb6e2
Clive Murray <hi+github@clivemurray.com>
2017-09-19 10:57:05 +0100

    show users on datasource page (#41)

--- app/routes/datasources.js
+++ app/routes/datasources.js
@@ -1,11 +1,13 @@
 module.exports = function(router, datasourceTools) {
   router.get('/datasources/show/:datasourceId', function (req, res) {
     var datasource = datasourceTools.getDatasource(req.params.datasourceId),
-      datasourceApps = datasourceTools.getDatasourceApps(req.params.datasourceId);
+      datasourceApps = datasourceTools.getDatasourceApps(req.params.datasourceId),
+      datasourceUsers = datasourceTools.getDatasourceUsers(req.params.datasourceId);
 
     res.render('datasources/show', {
       datasource: datasource,
-      datasourceApps: datasourceApps
+      datasourceApps: datasourceApps,
+      datasourceUsers: datasourceUsers
     });
   });
 }
