d240bbd13367261f3b6cd6011b34c4b9d3c2b7be
axemonkey <hi+github@clivemurray.com>
2017-09-21 11:36:08 +0100

    differentiate between user page and user home page

--- app/routes/users.js
+++ app/routes/users.js
@@ -2,12 +2,19 @@ module.exports = function(router, userTools) {
   router.get('/users/show/:userId', function (req, res) {
     var user = userTools.getUser(req.params.userId),
       userApps = userTools.getUserApps(req.params.userId),
-      userDatasources = userTools.getUserDatasources(req.params.userId);
+      userDatasources = userTools.getUserDatasources(req.params.userId),
+      data = req.session.data,
+      signedInUserId;
+
+    if(data && data.user_id) {
+      signedInUserId = parseInt(data.user_id, 10);
+    }
 
     res.render('users/show', {
       user: user,
       userApps: userApps,
-      userDatasources: userDatasources
+      userDatasources: userDatasources,
+      signedInUserId: signedInUserId
     });
   });
 }
