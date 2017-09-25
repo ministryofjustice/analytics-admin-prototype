9660e0134ef7ecaed3c00767b472023e8317c0d5
axemonkey <hi+github@clivemurray.com>
2017-09-25 17:25:42 +0100

    sorting out datasources and users in admin

--- app/modules/user-tools.js
+++ app/modules/user-tools.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var _ = require('lodash'),
+  arrayTools = require('../modules/array-tools.js'),
   users = require('../assets/data/dummy-users.json'),
   apps = require('../assets/data/dummy-apps.json'),
   datasources = require('../assets/data/dummy-datasources.json');
@@ -9,16 +10,20 @@ var userTools = {
   getUser: function(id) {
     return _.find(users, {'id': parseInt(id, 10)});
   },
-  getUserApps: function(id) {
+  getUserApps: function(id, invert) {
     var userApps = [],
       x,
       appGroup,
-      isMember;
+      isMember,
+      invertGroup = invert || false;
 
     for (x = 0; x < apps.length; x += 1) {
       appGroup = apps[x].appGroup;
 
       isMember = _.find(appGroup, {'id': parseInt(id, 10)});
+      if (invertGroup) {
+        isMember = !isMember;
+      }
       if (isMember) {
         userApps.push({
           id: apps[x].id,
@@ -30,6 +35,9 @@ var userTools = {
 
     return userApps;
   },
+  getAppsNotAvailableToUser: function(id) {
+    return this.getUserApps(id, true);
+  },
   getUserDatasources: function(userId) {
     var self = this,
       user = self.getUser(userId),
@@ -38,7 +46,7 @@ var userTools = {
       datasource;
 
     if (user.userDatasources.length) {
-      for (x = 0; x < user.userDatasources.length; x++) {
+      for (x = 0; x < user.userDatasources.length; x += 1) {
         datasource = _.find(datasources, {'id': parseInt(user.userDatasources[x], 10)});
 
         userDatasources.push({
@@ -49,6 +57,21 @@ var userTools = {
     }
 
     return userDatasources;
+  },
+  getDatasourcesNotAvailableToUser: function(userId) {
+    var self = this,
+      user = self.getUser(userId),
+      userDatasources = user.userDatasources,
+      x,
+      datasourcesNotAvailableToUser = [];
+
+    for (x = 0; x < datasources.length; x += 1) {
+      if(_.indexOf(userDatasources, x) === -1) {
+        datasourcesNotAvailableToUser.push(datasources[x]);
+      }
+    }
+
+    return datasourcesNotAvailableToUser;
   },
   updateUser: function(userId, formData) {
     var userIndex = _.findIndex(users, {'id': parseInt(userId, 10)});
@@ -73,6 +96,54 @@ var userTools = {
   },
   deleteUser: function(userId) {
     _.remove(users, {'id': parseInt(userId, 10)});
+
+    return true;
+  },
+  addUserToApp: function(userId, formData) {
+    var appId = parseInt(formData['add-user-to-app'], 10),
+      app = _.find(apps, {'id': appId}),
+      appIndex = _.findIndex(apps, {'id': appId}),
+      appGroup = app.appGroup;
+
+    appGroup.push({
+      id: parseInt(userId, 10),
+      role: 1
+    });
+
+    apps[appIndex].appGroup = _.sortBy(appGroup, 'id');
+
+    return true;
+  },
+  removeUserFromApp: function(userId, appId) {
+    var app = _.find(apps, {'id': parseInt(appId, 10)}),
+      appIndex = _.findIndex(apps, {'id': parseInt(appId, 10)}),
+      appGroup = app.appGroup;
+
+    appGroup = _.reject(appGroup, {'id': parseInt(userId, 10)});
+    apps[appIndex].appGroup = appGroup;
+
+    return true;
+  },
+  addDatasourceToUser: function(userId, formData) {
+    var self = this,
+      datasourceId = parseInt(formData['add-datasource-to-user'], 10),
+      user = self.getUser(userId),
+      userIndex = _.findIndex(users, {'id': parseInt(userId, 10)}),
+      datasources = user.userDatasources;
+
+    datasources.push(datasourceId);
+    users[userIndex].userDatasources = _.uniq(arrayTools.sort(datasources));
+
+    return true;
+  },
+  removeDatasourceFromUser: function(userId, datasourceId) {
+    var self = this,
+      user = self.getUser(userId),
+      userIndex = _.findIndex(users, {'id': parseInt(userId, 10)}),
+      datasources = user.userDatasources;
+
+    datasources = _.pull(datasources, parseInt(datasourceId, 10));
+    users[userIndex].datasources = datasources;
 
     return true;
   }
--- app/routes/admin/admin-datasources.js
+++ app/routes/admin/admin-datasources.js
@@ -6,11 +6,15 @@ module.exports = function(router, constants, datasources, datasourceTools) {
   });
   router.get('/admin/datasources/edit/:datasourceId', function (req, res) {
     var datasource = datasourceTools.getDatasource(req.params.datasourceId),
+      datasourceApps = datasourceTools.getDatasourceApps(req.params.datasourceId),
+      datasourceUsers = datasourceTools.getDatasourceUsers(req.params.datasourceId),
       appsWithoutDatasource = datasourceTools.getAppsWithoutDatasource(req.params.datasourceId),
       usersWithoutDatasource = datasourceTools.getUsersWithoutDatasource(req.params.datasourceId);
 
     res.render('admin/datasources/edit', {
       datasource: datasource,
+      datasourceApps: datasourceApps,
+      datasourceUsers: datasourceUsers,
       appsWithoutDatasource: appsWithoutDatasource,
       usersWithoutDatasource: usersWithoutDatasource
     });
@@ -44,7 +48,7 @@ module.exports = function(router, constants, datasources, datasourceTools) {
   });
   router.post('/admin/datasources/add-datasource-to-app/:datasourceId', function (req, res) {
     if(datasourceTools.addDatasourceToApp(req.params.datasourceId, req.body)) {
-      res.redirect('/admin/datasources/show/' + req.params.datasourceId);
+      res.redirect('/admin/datasources/edit/' + req.params.datasourceId);
     } else {
       console.log('add datasource to app failed');
       res.send('add datasource to app failed');
@@ -52,10 +56,26 @@ module.exports = function(router, constants, datasources, datasourceTools) {
   });
   router.post('/admin/datasources/add-datasource-to-user/:datasourceId', function (req, res) {
     if(datasourceTools.addDatasourceToUser(req.params.datasourceId, req.body)) {
-      res.redirect('/admin/datasources/show/' + req.params.datasourceId);
+      res.redirect('/admin/datasources/edit/' + req.params.datasourceId);
     } else {
       console.log('add datasource to user failed');
       res.send('add datasource to user failed');
+    }
+  });
+  router.get('/admin/datasources/remove-user/:datasourceId/:userId', function (req, res) {
+    if(datasourceTools.removeDatasourceUser(req.params.datasourceId, req.params.userId)) {
+      res.redirect('/admin/datasources/edit/' + req.params.datasourceId);
+    } else {
+      console.log('remove datasource from user failed');
+      res.send('remove datasource from user failed');
+    }
+  });
+  router.get('/admin/datasources/remove-app/:datasourceId/:appId', function (req, res) {
+    if(datasourceTools.removeDatasourceApp(req.params.datasourceId, req.params.appId)) {
+      res.redirect('/admin/datasources/edit/' + req.params.datasourceId);
+    } else {
+      console.log('add datasource to app failed');
+      res.send('add datasource to app failed');
     }
   });
   router.get('/admin/datasources/delete/:datasourceId', function (req, res) {
